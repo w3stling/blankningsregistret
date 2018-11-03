02b74bb5f30806fccb388ac14dd33500ffe41a00
Travis CI User <travis@example.org>
2018-11-03 07:31:06 +0000

    Publishing javadoc

---
+++ javadoc/2.0.0-SNAPSHOT/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.apptastic.blankningsregistret","l":"Blankningsregistret"},{"p":"com.apptastic.blankningsregistret.internal","l":"ExcelFileReader"},{"p":"com.apptastic.blankningsregistret","l":"NetShortPosition"}]
